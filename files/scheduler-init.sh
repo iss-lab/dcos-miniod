12c9942303fd833992ffb7aa048556630616f8e4
Drew Kerrigan <andrew.kerrigan@issgovernance.com>
2020-10-09 16:20:02 -0400

    Updating config system

--- files/scheduler-init.sh
+++ files/scheduler-init.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 [ ! -z ${CONFIG_MUSTACHE_BASE64} ] && \
-  rm -f ${MESOS_SANDBOX}/config.json.mustache && \
-  echo "${CONFIG_MUSTACHE_BASE64}" | base64 -d > ${MESOS_SANDBOX}/config.json.mustache
+  rm -f ${MESOS_SANDBOX}/config.env.mustache && \
+  echo "${CONFIG_MUSTACHE_BASE64}" | base64 -d > ${MESOS_SANDBOX}/config.env.mustache
